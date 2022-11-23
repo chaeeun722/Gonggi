dd8cae6b8b8d359901ee0e9df59fd34df73d625b
chaeeun <kimce0405@soongsil.ac.kr>
2022-11-23 22:28:29 +0900

    하단바 구현

---
+++ app/src/main/java/com/example/myapplication/MyPostListActivity.java
@@ -0,0 +1,31 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MyPostListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_post_list);
+
+        //게시판 클릭시 액태비티 전환
+        Button home_btn = (Button) findViewById(R.id.home);
+        home_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0,0); //애니메이션 없애는거
+                finish(); //원래 액티비티 종료
+            }
+        });
+        //여기까지
+
+    }
+}
